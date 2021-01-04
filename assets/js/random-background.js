3065c02f1e399ee300405f868b7a01cdf9062ad1
ahebrawi <76227677+ahebrawi@users.noreply.github.com>
2021-01-04 02:03:31 -0500

    rndom image

---
+++ assets/js/random-background.js
@@ -0,0 +1,9 @@
+var images = ['home2.jpg', 'home1.jpg', 'home3.jpg'];
+$('#home').css({'background': 'url(assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+$('#home').css({'background-attachment': 'fixed'});
+$('#home').css({'background-repeat': 'no-repeat'});
+$('#home').css({'background-position': '50% 50%'});
+$('#home').css({'-webkit-background-size': 'cover'});
+$('#home').css({'-moz-background-size': 'cover'});
+$('#home').css({'-o-background-size': 'cover'});
+$('#home').css({'background-size': 'cover'});
