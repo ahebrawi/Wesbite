d39aa6fd07f6ed1ae02f765041e9f1c7bcdbed38
ahebrawi <76227677+ahebrawi@users.noreply.github.com>
2021-01-06 18:16:32 -0500

    new images

--- assets/js/random-background.js
+++ assets/js/random-background.js
@@ -1,4 +1,4 @@
-var images = ['home2.jpg', 'home1.jpg', 'home3.jpg'];
+var images = ['home2.jpg', 'home1.jpg', 'home3.jpg', 'home4.jpg', 'home5.jpg', 'home6.jpg'];
 $('#home').css({'background': 'url(assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 $('#home').css({'background-attachment': 'fixed'});
 $('#home').css({'background-repeat': 'no-repeat'});
